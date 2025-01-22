ed692c76a5df3ef7af5496830de12825169da02b
Aditya Surapaneni <asurap1426@gmail.com>
2025-01-21 19:04:42 -0800

    Created a BASE_PATH variable and modified links to account for the GitHub directory in the webpage URL.

--- global.js
+++ global.js
@@ -12,13 +12,15 @@ let nav = document.createElement('nav');
 document.body.prepend(nav);
 
 const ARE_WE_HOME = document.documentElement.classList.contains('home');
+const BASE_PATH = '/portfolio';
 
 for (let p of pages) {
   let url = p.url;
   let title = p.title;
 
-  url = !ARE_WE_HOME && !url.startsWith('http') ? '../' + url : url;
-
+  if (!ARE_WE_HOME && !url.startsWith('http')) {
+    url = BASE_PATH + '/' + url;
+  }
   let a = document.createElement('a');
   a.href = url;
   a.textContent = title;
