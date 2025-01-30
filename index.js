2f46b96c09edc02f3c8841ca9fd6d2e0dd9fe5bc
Aditya Surapaneni <asurap1426@gmail.com>
2025-01-29 17:42:53 -0800

    Added code for GitHub profile statistics.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import { fetchJSON, renderProjects } from './global.js';
+import { fetchJSON, renderProjects, fetchGitHubData } from './global.js';
 
 const projects = await fetchJSON('/portfolio/lib/projects.json');
 const latestProjects = projects.slice(0, 3);
@@ -6,3 +6,18 @@ const latestProjects = projects.slice(0, 3);
 const projectsContainer = document.querySelector('.projects');
 
 renderProjects(latestProjects, projectsContainer, 'h2');
+
+const profileStats = document.querySelector('#profile-stats');
+
+const githubData = await fetchGitHubData('adityasurap');
+
+if (profileStats) {
+    profileStats.innerHTML = `
+          <dl>
+            <dt>Public Repos:</dt><dd>${githubData.public_repos}</dd>
+            <dt>Public Gists:</dt><dd>${githubData.public_gists}</dd>
+            <dt>Followers:</dt><dd>${githubData.followers}</dd>
+            <dt>Following:</dt><dd>${githubData.following}</dd>
+          </dl>
+      `;
+}
