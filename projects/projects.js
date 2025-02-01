26b3224c481343506ab9acaed52e29f2687784fb
Aditya Surapaneni <asurap1426@gmail.com>
2025-02-01 15:22:17 -0800

    Updated d3 import statement.

--- projects/projects.js
+++ projects/projects.js
@@ -1,4 +1,5 @@
 import { fetchJSON, renderProjects } from '../global.js';
+import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7/+esm";
 
 const projects = await fetchJSON('/portfolio/lib/projects.json');
 const projectsContainer = document.querySelector('.projects');
