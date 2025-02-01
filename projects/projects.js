1ad5eb9ffa1a1337e6940f4983c0f124ad462e11
Aditya Surapaneni <asurap1426@gmail.com>
2025-02-01 14:46:01 -0800

    Added a pie chart.

--- projects/projects.js
+++ projects/projects.js
@@ -1,4 +1,5 @@
 import { fetchJSON, renderProjects } from '../global.js';
+import * as d3 from 'd3';
 
 const projects = await fetchJSON('/portfolio/lib/projects.json');
 const projectsContainer = document.querySelector('.projects');
@@ -7,3 +8,21 @@ const projectsTitle = document.querySelector('.projects-title');
 projectsTitle.textContent = `${projects.length} Projects`;
 
 renderProjects(projects, projectsContainer, 'h2');
+
+let arcGenerator = d3.arc().innerRadius(0).outerRadius(50);
+
+let data = [1, 2, 3, 4, 5, 5];
+
+let sliceGenerator = d3.pie();
+let arcData = sliceGenerator(data);
+
+let colors = d3.scaleOrdinal(d3.schemeTableau10);
+
+let arcs = arcData.map((d) => arcGenerator(d));
+
+arcs.forEach((arc, idx) => {
+    d3.select('svg')
+      .append('path')
+      .attr('d', arc)
+      .attr('fill', colors[idx]);
+});
