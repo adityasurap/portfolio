628f98b5c38d06d662c07e9230c96272399d2f82
Aditya Surapaneni <asurap1426@gmail.com>
2025-02-11 12:09:50 -0800

    Restructured main.js.

--- meta/main.js
+++ meta/main.js
@@ -1,18 +1,10 @@
 let data = [];
 let commits = [];
 
-async function loadData() {
-    data = await d3.csv('loc.csv', (row) => ({
-      ...row,
-      line: Number(row.line),
-      depth: Number(row.depth),
-      length: Number(row.length),
-      date: new Date(row.date + 'T00:00' + row.timezone),
-      datetime: new Date(row.datetime),
-    }));
-
-    displayStats();
-}
+document.addEventListener('DOMContentLoaded', async () => {
+  await loadData();
+  createScatterplot();
+});
 
 function processCommits() {
     commits = d3
@@ -83,20 +75,41 @@ function displayStats() {
       dl.append('dd').text(maxPeriod);
 }
 
-document.addEventListener('DOMContentLoaded', async () => {
-  await loadData();
-  createScatterplot();
-});
+async function loadData() {
+  data = await d3.csv('loc.csv', (row) => ({
+    ...row,
+    line: Number(row.line),
+    depth: Number(row.depth),
+    length: Number(row.length),
+    date: new Date(row.date + 'T00:00' + row.timezone),
+    datetime: new Date(row.datetime),
+  }));
 
-const width = 1000;
-const height = 600;
+  displayStats();
+}
 
-const xScale = d3.scaleTime();
-const yScale = d3.scaleLinear();
+let xScale;
+let yScale;
 
 function createScatterplot() {
+  const width = 1000;
+  const height = 600;
   const margin = { top: 10, right: 10, bottom: 30, left: 20 };
+
+  const svg = d3
+    .select('#chart')
+    .append('svg')
+    .attr('viewBox', `0 0 ${width} ${height}`)
+    .style('overflow', 'visible');
+
+  xScale = d3
+        .scaleTime()
+        .domain(d3.extent(commits, (d) => d.datetime))
+        .range([0, width])
+        .nice();
   
+  yScale = d3.scaleLinear().domain([0, 24]).range([height, 0]);
+
   const usableArea = {
     top: margin.top,
     right: width - margin.right,
@@ -106,30 +119,21 @@ function createScatterplot() {
     height: height - margin.top - margin.bottom,
   };
 
-  xScale.domain(d3.extent(commits, (d) => d.datetime)).nice();
-  yScale.domain([0, 24]);
-
   xScale.range([usableArea.left, usableArea.right]);
   yScale.range([usableArea.bottom, usableArea.top]);
-
-  const xAxis = d3.axisBottom(xScale);
-  
-  const yAxis = d3
-    .axisLeft(yScale)
-    .tickFormat((d) => String(d % 24).padStart(2, '0') + ':00');
-
-  const svg = d3
-    .select('#chart')
-    .append('svg')
-    .attr('viewBox', `0 0 ${width} ${height}`)
-    .style('overflow', 'visible');
 
   const gridlines = svg
     .append('g')
     .attr('class', 'gridlines')
     .attr('transform', `translate(${usableArea.left}, 0)`);
   
-  gridlines.call(d3.axisLeft(yScale).tickFormat('').tickSize(-usableArea.width));
+    gridlines.call(d3.axisLeft(yScale).tickFormat('').tickSize(-usableArea.width));
+
+  const xAxis = d3.axisBottom(xScale);
+  
+  const yAxis = d3
+    .axisLeft(yScale)
+    .tickFormat((d) => String(d % 24).padStart(2, '0') + ':00');
 
   svg
     .append('g')
